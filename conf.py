9ee4de053a05b0bcd819aba4ee0aa7bed3eab6d1
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-14 12:31:38 +0530

    Title

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath("../"))
 
-project = 'Test'
+project = 'Documentation for Fabric'
 copyright = '2023, Paddy'
 author = 'Paddy'
 release = '1.0'
