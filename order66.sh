ff4a75005b2b60f4cf9aa2448c05301e090b4f78
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-15 10:54:28 +0530

    Crawler Mod

--- order66.sh
+++ order66.sh
@@ -4,6 +4,6 @@ printf 'n\n\nDocForFabric\n\nPaddy\n\n1\n\n' | sphinx-quickstart
 cd ..
 cp ./index.rst ./conf.py ./docs
 sphinx-apidoc -o docs .
-rm docs/conf.rst
+rm docs/conf.rst docs/crawler.rst
 cd docs/
 make html
