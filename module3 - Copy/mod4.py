2b9cd635a9b1da2367221b01411114f806b5874e
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-15 11:12:25 +0530

    Mod4 testing

---
+++ module3 - Copy/mod4.py
@@ -0,0 +1,18 @@
+def sample_function4():
+    """
+    TNever believe in something just because you want to believe it.
+    """
+    var = 1
+    SomeOtherValue = 117
+
+    return "Some higher meaning"
+
+
+def sample_function_returns_for_the_second_time():
+    """
+    And i, turn back the time, im at the chateau and i feel, alright.
+    """
+    var = 13
+    SomeOtherValue = 118
+
+    return "Dont let anything get in your way...try again....fail again....fail better"
