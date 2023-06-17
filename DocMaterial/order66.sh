7633638320d2ef610ef2da47d53225f18d275f31
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-17 10:47:45 +0530

    tree

--- DocMaterial/order66.sh
+++ DocMaterial/order66.sh
@@ -4,6 +4,7 @@ printf 'n\n\nDocForFabric\n\nPaddy\n\n1\n\n' | sphinx-quickstart
 cd ..
 cp ./DocMaterial/index.rst ./DocMaterial/conf.py ./docs
 sphinx-apidoc -o docs .
+tree
 rm docs/conf.rst docs/crawler.rst
 cd docs/
 make html
