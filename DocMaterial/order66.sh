1deaecca4b3d2fdbce2611f0e904e992659a82d9
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-17 17:51:15 +0530

    Target acquired

--- DocMaterial/order66.sh
+++ DocMaterial/order66.sh
@@ -3,7 +3,7 @@ cd docs/
 printf 'n\n\nDocForFabric\n\nPaddy\n\n1\n\n' | sphinx-quickstart
 cd ..
 cp ./DocMaterial/index.rst ./DocMaterial/conf.py ./docs
-sphinx-apidoc -o docs .
+sphinx-apidoc -o docs ./src_for_doc./AutoDocAction
 tree
 cat docs/conf.py
 # rm docs/conf.rst docs/crawler.rst
