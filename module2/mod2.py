801a7dfa3181490094448e798bd9c3d97037b15d
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-19 09:09:24 +0530

    Back to reality

--- module2/mod2.py
+++ module2/mod2.py
@@ -2,16 +2,7 @@ def sample_function2():
     """
     Calculates features i.e trend variables for 2,3,5,10 days
 
-    Parameters
-    ---------
-    group: DataFrame group
-        group of ticker_id
     
-    Returns
-    ------
-    df: Dataframe
-        dataframe for all features for ticker_ids
-
     """
     var = 1
     SomeOtherValue = 117
@@ -21,7 +12,7 @@ def sample_function2():
 
 def sample_function_returns_for_the_second_time():
     """
-    Improvise, Adapt, Overcome
+    Some things never change
     """
     var = 13
     SomeOtherValue = 118
