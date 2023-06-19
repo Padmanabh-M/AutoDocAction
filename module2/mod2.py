cdf7b4022530355b525ef0648090ed357cb04d2d
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-19 08:53:00 +0530

    Sample change

--- module2/mod2.py
+++ module2/mod2.py
@@ -2,15 +2,7 @@ def sample_function2():
     """
     Calculates features i.e trend variables for 2,3,5,10 days
 
-    Parameters
-    ---------
-    group: DataFrame group
-        group of ticker_id
     
-    Returns
-    ------
-    df: Dataframe
-        dataframe for all features for ticker_ids
     """
     var = 1
     SomeOtherValue = 117
