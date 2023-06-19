856b8bd7de92fd99071fcdfabe5bc258e106c0ff
Padmanabh Manikandan <77781626+Padmanabh-M@users.noreply.github.com>
2023-06-19 09:12:09 +0530

    Update mod2.py

--- module2/mod2.py
+++ module2/mod2.py
@@ -2,7 +2,16 @@ def sample_function2():
     """
     Calculates features i.e trend variables for 2,3,5,10 days
 
+    Parameters
+    ---------
+    group: DataFrame group
+        group of ticker_id
     
+    Returns
+    ------
+    df: Dataframe
+        dataframe for all features for ticker_ids
+        
     """
     var = 1
     SomeOtherValue = 117
@@ -12,7 +21,7 @@ def sample_function2():
 
 def sample_function_returns_for_the_second_time():
     """
-    Some things never change
+    Improvise, Adapt, Overcome
     """
     var = 13
     SomeOtherValue = 118
