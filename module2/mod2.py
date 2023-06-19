9268df60b58044f20a756271e28449b6d1ec452c
Padmanabh Manikandan <specter0117@gmail.com>
2023-06-19 09:02:52 +0530

    Sample change

--- module2/mod2.py
+++ module2/mod2.py
@@ -2,7 +2,15 @@ def sample_function2():
     """
     Calculates features i.e trend variables for 2,3,5,10 days
 
+    Parameters
+    ---------
+    group: DataFrame group
+        group of ticker_id
     
+    Returns
+    ------
+    df: Dataframe
+        dataframe for all features for ticker_ids
     """
     var = 1
     SomeOtherValue = 117
@@ -12,7 +20,7 @@ def sample_function2():
 
 def sample_function_returns_for_the_second_time():
     """
-    Some things never change
+    Changing for the better
     """
     var = 13
     SomeOtherValue = 118
