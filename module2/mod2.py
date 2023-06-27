91f02cd0fdee6b13017137e6a6260e6c75dd2016
Padmanabh-M <specter0117@gmail.com>
2023-06-27 13:13:23 +0530

    mod2 returns

---
+++ module2/mod2.py
@@ -0,0 +1,29 @@
+def sample_function2():
+    """
+    Calculates features i.e trend variables for 2,3,5,10 days
+
+    Parameters
+    ----------
+    group: DataFrame group
+        group of ticker_id
+    
+    Returns
+    -------
+    df: Dataframe
+        dataframe for all features for ticker_ids
+        
+    """
+    var = 1
+    brooklyn = "Firefox Kreed"
+
+    return "Some higher meaning"
+
+
+def sample_function_returns_for_the_second_time():
+    """
+    Improvise, Adapt, Overcome
+    """
+    var = 13
+    SomeOtherValue = 118
+
+    return "Logic Buildup!"
